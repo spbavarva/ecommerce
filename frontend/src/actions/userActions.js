b2d04161c6dee647e80087cd628bfb10233c4904
spbavarva <84507631+spbavarva@users.noreply.github.com>
2021-12-29 22:29:38 +0530

    first user commit

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,30 +1,30 @@
-import {
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  CLEAR_ERROR
-} from "../constants/userConstants";
-import axios from "axios";
+// import {
+//   LOGIN_REQUEST,
+//   LOGIN_SUCCESS,
+//   LOGIN_FAIL,
+//   CLEAR_ERROR
+// } from "../constants/userConstants";
+// import axios from "axios";
 
-export const login = (email, password) => async (dispatch) => {
-  try {
-    dispatch({ type: LOGIN_REQUEST });
+// export const login = (email, password) => async (dispatch) => {
+//   try {
+//     dispatch({ type: LOGIN_REQUEST });
 
-    const config = { header: { "Content-Type": "application/json" } };
+//     const config = { header: { "Content-Type": "application/json" } };
 
-    const { data } = await axios.post(
-        `/api/v1/login`,
-        { email, password },
-        config
-        );
-        console.log(data);
+//     const { data } = await axios.post(
+//         `/api/v1/login`,
+//         { email, password },
+//         config
+//         );
+//         console.log(data);
 
-    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
-  } catch (error) {
-    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
-  }
-};
+//     dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+//   } catch (error) {
+//     dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+//   }
+// };
 
-export const clearError = () => async (dispatch) => {
-  dispatch({ type: CLEAR_ERROR });
-};
+// export const clearError = () => async (dispatch) => {
+//   dispatch({ type: CLEAR_ERROR });
+// };
